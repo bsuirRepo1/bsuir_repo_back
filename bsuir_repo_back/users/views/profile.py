a5e454802cf2c438700f3d414ee9638dc8a6926c
KatiaShauro <katia.shauro2@gmail.com>
2024-10-03 23:54:18 +0300

    little fixes

--- bsuir_repo_back/users/views/profile.py
+++ bsuir_repo_back/users/views/profile.py
@@ -20,12 +20,12 @@ class ProfileViewSet(viewsets.ModelViewSet):
     def get_queryset(self):
         user = self.request.user
         if user.is_superuser:
-            users = UserProfile.objects.all()
+            users = UserProfile.objects.select_related('user')
 
             return users
 
         if not user.is_superuser:
-            profile = UserProfile.objects.filter(user=user)
+            profile = UserProfile.objects.filter(user=user).only('first_name', 'last_name', 'faculty')
 
             return profile
 
