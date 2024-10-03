6231f3fd43cc3b4f86ca481a8062b7e60a78dc9a
KatiaShauro <katia.shauro2@gmail.com>
2024-10-03 23:24:16 +0300

    Profile API was added

--- bsuir_repo_back/users/serializers/profile_serializer.py
+++ bsuir_repo_back/users/serializers/profile_serializer.py
@@ -7,3 +7,22 @@ class ProfileSerializer(serializers.ModelSerializer):
     class Meta:
         model = UserProfile
         fields = '__all__'
+
+
+class ProfilePatchSerializer(serializers.Serializer):
+    photo = serializers.CharField(required=False)
+    about = serializers.CharField(required=False, max_length=1000)
+    city = serializers.CharField(required=False, max_length=30)
+
+    def validate(self, attrs):
+        if not any([attrs.get('about'), attrs.get('city'), attrs.get('photo')]):
+            raise serializers.ValidationError("At least one field must be provided.")
+        return attrs
+
+    def update(self, instance, validated_data):
+        instance.photo = validated_data.get('photo', instance.photo)
+        instance.city = validated_data.get('city', instance.city)
+        instance.about = validated_data.get('about', instance.about)
+        instance.save()
+
+        return instance
