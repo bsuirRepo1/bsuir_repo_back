0c2f5b05c4aac9345b8b73e22efcb1dd807d2c35
KatiaShauro <katia.shauro2@gmail.com>
2024-10-08 22:44:39 +0300

    days with service was fixed

--- bsuir_repo_back/users/serializers/profile_serializer.py
+++ bsuir_repo_back/users/serializers/profile_serializer.py
@@ -1,12 +1,19 @@
+from django.utils import timezone
 from rest_framework import serializers
-
 from users.models import UserProfile
 
 
 class ProfileSerializer(serializers.ModelSerializer):
+    days_in_system = serializers.SerializerMethodField()
+
     class Meta:
         model = UserProfile
-        fields = '__all__'
+        exclude = ['days_with_service']
+
+    def get_days_in_system(self, obj):
+        current_date = timezone.now()
+        delta = current_date - obj.days_with_service
+        return delta.days
 
 
 class ProfilePatchSerializer(serializers.Serializer):
