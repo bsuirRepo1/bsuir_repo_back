90e5aa3343325b2d8e64aebc3ba8a1c46bf75fc5
DenisHrtn <denispython16@gmail.com>
2024-10-15 11:16:11 +0300

    postgres settings in docker-compose

--- bsuir_repo_back/bsuir_repo_core/settings.py
+++ bsuir_repo_back/bsuir_repo_core/settings.py
@@ -59,8 +59,12 @@ ASGI_APPLICATION = "bsuir_repo_core.asgi.application"
 
 DATABASES = {
     "default": {
-        "ENGINE": "django.db.backends.sqlite3",
-        "NAME": BASE_DIR / "db.sqlite3",
+        "ENGINE": "django.db.backends.postgresql",
+        "NAME": config('POSTGRES_NAME'),
+        "USER": config('POSTGRES_USER'),
+        "PASSWORD": config('POSTGRES_PASSWORD'),
+        "HOST": config('POSTGRES_HOST'),
+        "PORT": config('POSTGRES_PORT')
     }
 }
 
